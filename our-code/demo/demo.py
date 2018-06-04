73fdedb2b154b161ec3241f34185ab1b1f0df4ea
mateusnbm <mnbm@cin.ufpe.br>
2018-06-04 00:39:25 -0300

    Measuring times and reduction rates.

--- our-code/demo/demo.py
+++ our-code/demo/demo.py
@@ -4,6 +4,7 @@
 
 
 import os
+import time
 import random
 
 import algorithms.knn
@@ -15,7 +16,7 @@ Parameters.
 
 folds = 5
 
-data_filename = 'sample-data/poker-data-small.txt'
+data_filename = 'sample-data/poker-data.txt'
 header_filename = 'sample-data/poker-header.txt'
 
 data_class_index = 10
@@ -23,23 +24,49 @@ data_nominal_indexes = []
 data_numerical_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 configurations = [
-	(2, 3, 'am'),
-	(2, 3, 'sam'),
+	(2,  3, 'am'),
+	(2,  5, 'am'),
+	(2, 10, 'am'),
+	(3,  3, 'am'),
+	(3,  5, 'am'),
+	(3, 10, 'am'),
+	(4,  3, 'am'),
+	(4,  5, 'am'),
+	(5,  3, 'am'),
+	(5,  5, 'am'),
+	(7,  3, 'am'),
+	(9,  3, 'am'),
+	(2,  3, 'sam'),
+	(2,  5, 'sam'),
+	(2, 10, 'sam'),
+	(3,  3, 'sam'),
+	(3,  5, 'sam'),
+	(3, 10, 'sam'),
+	(4,  3, 'sam'),
+	(4,  5, 'sam'),
+	(5,  3, 'sam'),
+	(5,  5, 'sam'),
+	(7,  3, 'sam'),
+	(9,  3, 'sam')
 ]
 
 
 '''
-Create temporary folders to store data folds and prototypes.
+Create temporary folders to store data folds, prototypes, times and reduction rates.
 '''
 
 print('')
 print('Creating temporary data folders...')
 
 folds_path = 'tmp/folds/'
+times_path = 'tmp/times/'
+rates_path = 'tmp/rates/'
 prototypes_path = 'tmp/prototypes/'
 
 os.system('rm -rf tmp')
 os.makedirs(folds_path, exist_ok=True)
+os.makedirs(times_path, exist_ok=True)
+os.makedirs(rates_path, exist_ok=True)
 os.makedirs(prototypes_path, exist_ok=True)
 
 
@@ -121,9 +148,10 @@ for i in range(folds):
 
 			training_file.write(line)
 
-
 '''
-Close training files, we no longer perform I/O with them here.
+Close training files, we no longer perform I/O with them here. Closing these
+files is very important in order to avoid file handling issues, because CHI-PG
+will read from them while generating prototypes.
 '''
 
 for i in range(folds):
@@ -136,8 +164,14 @@ Generate prototypes for each data fold and configuration.
 '''
 
 print('Generating prototypes...')
+print('')
+
+times_filename = times_path + 'times.txt'
+times_file = open(times_filename, 'a')
 
 for configuration in configurations:
+
+	average_time = 0
 
 	fs = configuration[0]
 	min = configuration[1]
@@ -161,8 +195,78 @@ for configuration in configurations:
 		argument += str(fs) + ' ' + str(min) + ' ' + aggr + ' '
 		argument += training_filename + ' ' + header_filename + ' ' + output_filename
 
+		start_time = time.time()
+
 		os.system(argument)
 
+		average_time += time.time() - start_time
+
+	t = (average_time/folds)
+	t = '{:.0f}:{:.0f}:{:.0f}'.format(t // 3600, (t % 3600 // 60), t % 60)
+
+	output  = 'Configuration '
+	output += str(fs) + 'FS '
+	output += 'min' + str(min) + ' '
+	output += aggr.upper() + ': '
+	output += t + '\n'
+
+	print(output)
+
+	times_file.write(output)
+
+times_file.close()
+
+'''
+Measure reduction rates.
+'''
+
+print('Computing reduction rates...')
+print('')
+
+rates_filename = rates_path + 'rates.txt'
+rates_file = open(rates_filename, 'a')
+
+for k in range(len(configurations)):
+
+	average_rate = 0
+
+	fs = configurations[k][0]
+	min = configurations[k][1]
+	aggr = configurations[k][2]
+
+	for i in range(folds):
+
+		training_file = open(paths[i][0][1], 'r')
+		prototypes_fold_file = open(paths[i][2][k], 'r')
+
+		training_file.seek(0, 0)
+		for w, l in enumerate(training_file): pass
+		training_file_length = w + 1
+
+		prototypes_fold_file.seek(0, 0)
+		for w, l in enumerate(prototypes_fold_file): pass
+		prototypes_fold_file_length = w + 1
+
+		p = prototypes_fold_file_length / training_file_length
+		average_rate += ((1 - p) * 100)
+
+		training_file.close()
+		prototypes_fold_file.close()
+
+	output  = 'Configuration '
+	output += str(fs) + 'FS '
+	output += 'min' + str(min) + ' '
+	output += aggr.upper() + ': '
+	output += '{:.2f}'.format(average_rate/folds)
+	output += '%\n'
+
+	print(output)
+
+	rates_file.write(output)
+
+rates_file.close()
+
+exit(0)
 
 '''
 Measure accuracies.
